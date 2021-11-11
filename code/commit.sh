52c35684c592c868a28d82183fc88d038ab06781
calzateu <calzateu@eafit.edu.co>
2021-11-11 15:04:48 -0500

    Dataset with sentiments

---
+++ code/commit.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#exec 3>&1 4>&2
+#trap 'exec 2>&4 1>&3' 0 1 2 3
+#exec 1>log.out 2>&1
+exec > log.out 2>&1
+set -x
+
+
+FECHA=$(date)
+
+cd /home/cristian/Descargas/Universidad/2021-2/Análisis_numérico/PROYECTO/Código/Numerical_Analysis && git add .
+cd /home/cristian/Descargas/Universidad/2021-2/Análisis_numérico/PROYECTO/Código/Numerical_Analysis && git commit -m $1
+cd /home/cristian/Descargas/Universidad/2021-2/Análisis_numérico/PROYECTO/Código/Numerical_Analysis && git pull
+cd /home/cristian/Descargas/Universidad/2021-2/Análisis_numérico/PROYECTO/Código/Numerical_Analysis && git push
